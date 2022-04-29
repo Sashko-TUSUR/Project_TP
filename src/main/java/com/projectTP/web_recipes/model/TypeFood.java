cfd8c52df76e7df4d25a2ec4938f56113317212c
Sashko-TUSUR <81729117+Sashko-TUSUR@users.noreply.github.com>
2022-04-29 22:04:13 +0700

    поправил

---
+++ src/main/java/com/projectTP/web_recipes/model/TypeFood.java
@@ -0,0 +1,29 @@
+package com.projectTP.web_recipes.model;
+
+import javax.persistence.*;
+
+@Entity
+
+public class TypeFood {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
